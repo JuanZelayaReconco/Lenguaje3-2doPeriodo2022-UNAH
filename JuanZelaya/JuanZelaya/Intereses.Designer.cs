ab4e5098b18bc6caeb723fd7340e5dfff2aefa7b
JuanZelayaReconco <juanzelayaunah@outlook.es>
2022-06-17 13:53:35 -0600

    Primer Commit
    Examen #1 de LPIII, 2do Periodo 2022 UNAH.

---
+++ JuanZelaya/JuanZelaya/Intereses.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace JuanZelaya
+{
+    partial class Intereses
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InteresListBox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.CapitalTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.TasaTextBox = new System.Windows.Forms.TextBox();
+            this.CalcularButton = new System.Windows.Forms.Button();
+            this.CancelarButton = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.TipoTasaComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(241, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bancos \"La Embajadora de Hierro\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre del cliente: ";
+            // 
+            // InteresListBox
+            // 
+            this.InteresListBox.FormattingEnabled = true;
+            this.InteresListBox.Location = new System.Drawing.Point(22, 191);
+            this.InteresListBox.Name = "InteresListBox";
+            this.InteresListBox.Size = new System.Drawing.Size(336, 290);
+            this.InteresListBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(281, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Aplicación para cálculo de intereses";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "No. de cuenta: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(427, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Capital: L.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "01-222-11889";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(136, 153);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(188, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Bartolomé de las Casas";
+            // 
+            // CapitalTextBox
+            // 
+            this.CapitalTextBox.Location = new System.Drawing.Point(487, 117);
+            this.CapitalTextBox.Name = "CapitalTextBox";
+            this.CapitalTextBox.ReadOnly = true;
+            this.CapitalTextBox.Size = new System.Drawing.Size(181, 20);
+            this.CapitalTextBox.TabIndex = 8;
+            this.CapitalTextBox.Text = "200,000.00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(380, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Fecha del depósito: ";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Enabled = false;
+            this.dateTimePicker1.Location = new System.Drawing.Point(487, 153);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 11;
+            this.dateTimePicker1.Value = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(487, 195);
+            this.dateTimePicker2.MinDate = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 13;
+            this.dateTimePicker2.Value = new System.DateTime(2023, 1, 1, 0, 0, 0, 0);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(364, 202);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(120, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Fecha posible de retiro: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(395, 239);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Tasa de interés: ";
+            // 
+            // TasaTextBox
+            // 
+            this.TasaTextBox.Location = new System.Drawing.Point(487, 232);
+            this.TasaTextBox.Name = "TasaTextBox";
+            this.TasaTextBox.ReadOnly = true;
+            this.TasaTextBox.Size = new System.Drawing.Size(51, 20);
+            this.TasaTextBox.TabIndex = 15;
+            this.TasaTextBox.Text = "1.5";
+            // 
+            // CalcularButton
+            // 
+            this.CalcularButton.Location = new System.Drawing.Point(398, 341);
+            this.CalcularButton.Name = "CalcularButton";
+            this.CalcularButton.Size = new System.Drawing.Size(75, 23);
+            this.CalcularButton.TabIndex = 16;
+            this.CalcularButton.Text = "Calcular";
+            this.CalcularButton.UseVisualStyleBackColor = true;
+            this.CalcularButton.Click += new System.EventHandler(this.CalcularButton_Click);
+            // 
+            // CancelarButton
+            // 
+            this.CancelarButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelarButton.Location = new System.Drawing.Point(612, 341);
+            this.CancelarButton.Name = "CancelarButton";
+            this.CancelarButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelarButton.TabIndex = 17;
+            this.CancelarButton.Text = "Cancelar";
+            this.CancelarButton.UseVisualStyleBackColor = true;
+            this.CancelarButton.Click += new System.EventHandler(this.CancelarButton_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(544, 237);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(18, 15);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "%";
+            // 
+            // TipoTasaComboBox
+            // 
+            this.TipoTasaComboBox.Enabled = false;
+            this.TipoTasaComboBox.FormattingEnabled = true;
+            this.TipoTasaComboBox.Items.AddRange(new object[] {
+            "Diario",
+            "Semanal",
+            "Mensual",
+            "Trimestral",
+            "Semestral",
+            "Anual"});
+            this.TipoTasaComboBox.Location = new System.Drawing.Point(566, 232);
+            this.TipoTasaComboBox.Name = "TipoTasaComboBox";
+            this.TipoTasaComboBox.Size = new System.Drawing.Size(121, 21);
+            this.TipoTasaComboBox.TabIndex = 19;
+            this.TipoTasaComboBox.Text = "Mensual";
+            // 
+            // Intereses
+            // 
+            this.AcceptButton = this.CalcularButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelarButton;
+            this.ClientSize = new System.Drawing.Size(742, 499);
+            this.Controls.Add(this.TipoTasaComboBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.CancelarButton);
+            this.Controls.Add(this.CalcularButton);
+            this.Controls.Add(this.TasaTextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.CapitalTextBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.InteresListBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Intereses";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cálculo de Intereses";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox InteresListBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox CapitalTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox TasaTextBox;
+        private System.Windows.Forms.Button CalcularButton;
+        private System.Windows.Forms.Button CancelarButton;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox TipoTasaComboBox;
+    }
+}
+
---
+++ JuanZelaya/JuanZelaya/Intereses.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuanZelaya
+{
+    public partial class Intereses : Form
+    {
+        public Intereses()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularButton_Click(object sender, EventArgs e)
+        {
+            InteresListBox.Items.Clear(); //Limpia la listbox en caso que hayan registros previamente.
+
+            //Inicialización de variables.
+            double capital = Convert.ToDouble(CapitalTextBox.Text);
+            double tasa = (Convert.ToDouble(TasaTextBox.Text)/100);
+            DateTime fechaDeposito = dateTimePicker1.Value;
+            DateTime fechaPosibleRetiro = dateTimePicker2.Value;
+
+            TimeSpan convertTiempo = fechaPosibleRetiro.Subtract(fechaDeposito); //Esta instrucción sirve para sacar la
+                                                                          //diferencia en días entre dos fechas.
+
+            int tiempoMeses=Convert.ToInt32(convertTiempo.Days)/30; //Convertimos los días a meses, basandonos en el
+                                                                    //formato de interés comercial (el año consta de
+                                                                    //360 días y cada mes se asume que contiene 30 días).
+
+            InteresListBox.Items.Add(tiempoMeses + " meses de generación de intereses: ");
+            InteresListBox.Items.Add("...");
+
+            for (int j = 1; j <= tiempoMeses; j++) //Se inicializa "j" en 1 para tomar en consideración
+                                                   //que los intereses no se generan el mes en el que
+                                                   //fue el depósito, sino hasta el mes siguiente.
+            {
+                //Se captura el valor almacenado en la variable de la fecha del depósito (fechaDeposito) y se le va
+                //aumentar en meses el valor que tenga la variable del control del ciclo, osea "j".
+                //El formato "MMMM" en los tipos de archivos "DateTime", nos ayuda a mostrar en pantalla el nombre
+                //del mes con respecto al su respectivo número.
+                InteresListBox.Items.Add("En "+fechaDeposito.AddMonths(j).ToString("MMMM")+" del "+ 
+                                        fechaDeposito.AddMonths(j).Year.ToString() + " generará: " +
+                                        "L. "+ CalcularInteres(capital, tasa, j).ToString("N") + " " +
+                                        "en intereses.");
+            }
+            InteresListBox.Items.Add("...");
+
+            capital += CalcularInteres(capital, tasa, tiempoMeses); //Se suman los intereses ganados segun la fecha
+                                                                    //del posible retiro al capital inicial.
+            InteresListBox.Items.Add("Monto futuro: L. "+capital.ToString("N"));
+        }
+
+        private void CancelarButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        //Función que nos ayudara a calcular el interes, de manera mensual.
+        private double CalcularInteres(double c, double i , double t)
+        {
+            double _I;
+
+            return _I = c * i * t;
+        }
+    }
+}
